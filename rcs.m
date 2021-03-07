4a47441ba8ee3d60ebc8820dec1e559b3991d22d
vishal-vardhan <vishaladepu007@gmail.com>
2021-03-07 14:36:36 +0530

    changed the variable names in rcs and wireDipoleAntenna

--- rcs.m
+++ rcs.m
@@ -23,37 +23,37 @@ function [sigma_, alpha_] = rcs(lambda_,a_,N_,printData_)
     E_inc_ = zeros(N_,1); %incident electric field
     Z_ = zeros(N_,N_);
 
-    for m = 1:N_
-        for n = 1:N_
-            rmn_ = abs(2*a_*sin(pi*(m-n)/N_));
+    for m_ = 1:N_
+        for n_ = 1:N_
+            rmn_ = abs(2*a_*sin(pi*(m_-n_)/N_));
             t_ = k_*rmn_;
             H_ = (1- (t_*t_/4)) - j*( ((2/pi)*log(g_*t_/2)) + (t_*t_/(2*pi)*(1 - log(g_*t_/2))) );
-            Z_(m,n) = (2*pi/lambda_)*(eta0_/4)*(w_)*H_;
+            Z_(m_,n_) = (2*pi/lambda_)*(eta0_/4)*(w_)*H_;
             
-            if m==n
-                Z_(m,n) = (k_*eta0_*2*pi*a_/(4*N_))*(1-(j*(2/pi)*(log(g_*k_*2*pi*a_/(4*N_))-1)));
+            if m_ == n_
+                Z_(m_,n_) = (k_*eta0_*2*pi*a_/(4*N_))*(1-(j*(2/pi)*(log(g_*k_*2*pi*a_/(4*N_))-1)));
             end
         end
     end
     
     phi_ = linspace(0,N_-1,N_)*(2*pi/N_);
-    for m = 1:N_
-        E_inc_(m) = exp(j*k_*(a_*cos(phi_(m)-phii_)));
+    for m_ = 1:N_
+        E_inc_(m_) = exp(j*k_*(a_*cos(phi_(m_)-phii_)));
     end
     alpha_ = pinv(Z_)*E_inc_;
     
     sigma_ = zeros(N_,1);
     
-    for m=1:N_
+    for m_=1:N_
         temp_ = 0;
-        an_ = phi_(m);
-        for n = 1:N_
-            ang_ = k_*a_*cos(phi_(n)-an_);
-            temp_ = temp_ + (alpha_(n)*w_*(cos(ang_) + j*sin(ang_)));
+        an_ = phi_(m_);
+        for n_ = 1:N_
+            ang_ = k_*a_*cos(phi_(n_)-an_);
+            temp_ = temp_ + (alpha_(n_)*w_*(cos(ang_) + j*sin(ang_)));
         end
         temp_ = temp_* temp_;
         
-        sigma_(m) = k_*eta0_*eta0_*temp_/4;
+        sigma_(m_) = k_*eta0_*eta0_*temp_/4;
     end
 %     polarplot(phi_,abs(sigma_));
     if(printData_ == "Yes")
